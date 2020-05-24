c81c7f7d8937619e1a124265da5b3b5b50d157f9
danyg <dalgo86@gmail.com>
2020-05-24 07:29:47 +0200

    unit tests

---
+++ test-helpers/run-unit-tests.ts
@@ -0,0 +1,9 @@
+import { createMochaRunner } from './suite/create-mocha-runner';
+import { testPath } from './test-paths';
+
+const runTests = createMochaRunner(testPath, '**/*.test.js');
+
+runTests().catch(() => {
+  console.error('Failed to run tests');
+  process.exit(1);
+});
---
+++ test/extension.test.ts
@@ -0,0 +1,11 @@
+import * as expect from 'expect';
+
+describe('this is just a test of the test', () => {
+  it('should pass', () => {
+    // given();
+
+    // when();
+
+    expect(true).toBe(true);
+  });
+});
