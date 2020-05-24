3c40755203edc58234492fde43ebecf3a0bee4f3
danyg <dalgo86@gmail.com>
2020-05-24 09:07:24 +0200

    refactor stripping out vscode from core logic

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,73 @@
 import * as vscode from 'vscode';
 
-console.log('Go To Test Loaded!');
+export interface UI {
+  info(message: string): void;
+}
 
-export function activate(context: vscode.ExtensionContext): void {
-  console.log('Go To Test ACTIVATED!');
-  vscode.window.showInformationMessage('Go To Test ACTIVATED!');
-  const disposable = vscode.commands.registerCommand('danyg-go-to-test.goToTest', async () => {
+class VSCodeUI implements UI {
+  info(message: string): void {
+    console.log(`[INFO]: ${message}`);
+    vscode.window.showInformationMessage(message);
+  }
+}
+
+export class GoToTest {
+  public constructor(private ui: UI) {
+    console.log('Go To Test Loaded!');
+  }
+
+  public activate(context: vscode.ExtensionContext) {
+    this.ui.info('Go To Test ACTIVATED!');
+
+    const disposable = this.registerTheCommand();
+
+    context.subscriptions.push(disposable);
+  }
+
+  private registerTheCommand() {
+    return vscode.commands.registerCommand(
+      'danyg-go-to-test.goToTest',
+      this.executeCommand.bind(this)
+    );
+  }
+
+  public async executeCommand() {
     console.log('Go To Test executed!');
-    const currentFile = vscode.window.activeTextEditor!.document.fileName;
-    const testFile = currentFile.replace(
+    const currentFile = this.getActiveTextEditorFilePath();
+    const testFile = this.getTestFile(currentFile);
+
+    await this.openFileInEditor(testFile);
+  }
+
+  private getActiveTextEditorFilePath() {
+    return vscode.window.activeTextEditor!.document.fileName;
+  }
+
+  private async openFileInEditor(testFile: vscode.Uri) {
+    this.ui.info(`Opening: ${testFile.fsPath}`);
+    const doc: vscode.TextDocument = await vscode.workspace.openTextDocument(testFile);
+    await vscode.window.showTextDocument(doc);
+    this.ui.info(`${testFile.fsPath} Opened`);
+  }
+
+  private getTestFile(srcFilePath: string) {
+    return vscode.Uri.file(this.getTestFilePath(srcFilePath));
+  }
+
+  private getTestFilePath(srcFilePath: string): string {
+    return srcFilePath.replace(
       /^(?<projectPath>.*)src(?<moduleInternalPath>.*)\.(?<ext>[tj]sx?)$/,
       '$<projectPath>test$<moduleInternalPath>.test.$<ext>'
     );
+  }
 
-    try {
-      console.log(`Opening: ${testFile}`);
-      vscode.window.showInformationMessage(`Opening: ${testFile}`);
-      const doc: vscode.TextDocument = await vscode.workspace.openTextDocument(
-        vscode.Uri.file(testFile)
-      );
-      await vscode.window.showTextDocument(doc);
-      vscode.window.showInformationMessage(`${testFile} Opened`);
-      console.log(`Opened!`);
-    } catch (e) {
-      console.error(`Error occurred!`, e);
-    }
-  });
-
-  context.subscriptions.push(disposable);
+  private info(message: string) {
+    console.log(`[INFO]: ${message}`);
+    vscode.window.showInformationMessage(message);
+  }
 }
 
+const goToTestExtension = new GoToTest(new VSCodeUI());
+
+export const activate = goToTestExtension.activate.bind(goToTestExtension);
 // export function deactivate() {}
--- test/extension.u.test.ts
+++ test/extension.u.test.ts
@@ -1,10 +1,23 @@
 import * as expect from 'expect';
+import { GoToTest, UI } from '../src/extension';
+import { mock } from 'ts-mockito';
+
+class UIDouble implements UI {
+  info(message: string): void {
+    console.log(message);
+    throw new Error('Method not implemented.');
+  }
+}
 
 describe('this is just a test of the test', () => {
-  it('should pass', () => {
+  it('should pass', async () => {
+    const ui: UI = mock(UIDouble);
+    const testSubject = new GoToTest(ui);
     // given();
 
     // when();
+
+    await testSubject.executeCommand();
 
     expect(true).toBe(true);
   });
