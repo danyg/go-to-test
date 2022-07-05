a3eaa20ddbf62eada03c065b65582ff8e90fc6ef
danyg <dalgo86@gmail.com>
2022-07-05 11:22:45 +0200

    [#7] MavenStrategy Go To Source

--- src/strategies/maven-strategy.ts
+++ src/strategies/maven-strategy.ts
@@ -1,14 +1,20 @@
 import Strategy from 'interfaces/strategy';
 
+const sourcePattern = /^(?<projectPath>.*)src(?<dirSep>[\\\/])main(?<moduleInternalPath>.*)\.(?<ext>[^\.]+)$/;
+const testPattern = /^(?<projectPath>.*)src(?<dirSep>[\\\/])test(?<moduleInternalPath>.*)Test\.(?<ext>[^\.]+)$/;
+
+const sourceTemplate = '$<projectPath>src$<dirSep>main$<moduleInternalPath>.$<ext>';
+const testTemplate = '$<projectPath>src$<dirSep>test$<moduleInternalPath>Test.$<ext>';
+
 export default class MavenStrategy implements Strategy {
   public static getInstance() {
     return new MavenStrategy();
   }
 
   resolve(filePath: string): string {
-    return filePath.replace(
-      /^(?<projectPath>.*)src(?<dirSep>[\\\/])main(?<moduleInternalPath>.*)\.(?<ext>[^\.]+)$/,
-      '$<projectPath>src$<dirSep>test$<moduleInternalPath>Test.$<ext>'
-    );
+    if (filePath.match(testPattern)) {
+      return filePath.replace(testPattern, sourceTemplate);
+    }
+    return filePath.replace(sourcePattern, testTemplate);
   }
 }
