4a9351d3d3cf314ec0c935ed6ea9512a3f53e089
danyg <dalgo86@gmail.com>
2022-07-05 11:14:47 +0200

    [#7] SameDirectory GoToSource

--- src/strategies/same-directory-strategy.ts
+++ src/strategies/same-directory-strategy.ts
@@ -1,11 +1,19 @@
 import Strategy from 'interfaces/strategy';
 
+const testPattern = /^(?<filePath>.*)\.test\.(?<ext>[^\.]+)$/;
+const sourcePattern = /^(?<filePath>.*)\.(?<ext>[^\.]+)$/;
+
+const sourceTemplate = '$<filePath>.$<ext>';
+const testTemplate = '$<filePath>.test.$<ext>';
 export default class SameDirectoryStrategy implements Strategy {
   public static getInstance() {
     return new SameDirectoryStrategy();
   }
 
   resolve(filePath: string): string {
-    return filePath.replace(/^(?<filePath>.*)\.(?<ext>[^\.]+)$/, '$<filePath>.test.$<ext>');
+    if (filePath.match(testPattern)) {
+      return filePath.replace(testPattern, sourceTemplate);
+    }
+    return filePath.replace(sourcePattern, testTemplate);
   }
 }
